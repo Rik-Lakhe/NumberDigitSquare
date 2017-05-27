e07a8ccd2df850f5fb74c889c888b7e9bd5c1547
riklakhe <rikeshlakhe@gmail.com>
2017-05-27 17:58:58 +0545

    compressed

--- number1.js
+++ number1.js
@@ -1,13 +1,1 @@
-var numb=prompt("helo from 2");
-var number = Number(numb);
-	temp=0;temp2=0;
-
-while(number>=1){
-	temp=number%10
-	console.log(temp);
-	temp2+=Math.pow(temp,2);
-	number=number/10;
-	number=parseInt(number);
-}
-
-document.write('<h1>result form number1.js = '+temp2+'</h1>');
+var numb=prompt("helo from 2"),number=Number(numb);for(temp=0,temp2=0;number>=1;)temp=number%10,console.log(temp),temp2+=Math.pow(temp,2),number/=10,number=parseInt(number);document.write("<h1>result form number1.js = "+temp2+"</h1>");
